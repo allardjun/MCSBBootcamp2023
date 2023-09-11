58da544d6203c60ae365d544c66ed13e4514167a
Jun Allard <jun.allard@uci.edu>
2023-09-11 15:33:47 -0700

    phase plane for FHN

---
+++ Module4/phaseplaneFHN.m
@@ -0,0 +1,32 @@
+% model parameters
+eps = 0.08;
+a = 1.00;
+b = 0.2;
+
+% model definition
+f = @(v,w) v - 1/3*v.^3 - w;
+g = @(v,w) eps*(v + a -b*w);
+
+%% single cell
+dxdt =@ (t,x) [f(x(1),x(2)); g(x(1),x(2));];
+
+% solve!
+[T,X] = ode45(dxdt,[0,100], [-0,-0.5]);
+
+
+
+figure(4); hold on;
+set(gca, 'xlim', [-2.5, 2.5], 'ylim', [-2.5,2.5])
+ylabel('w');
+xlabel('v')
+
+uArray = linspace(-2.5, 2.5,32);
+wArray = linspace(-2.5, 2.52,32);
+
+[uMesh,wMesh] = meshgrid(uArray, wArray);
+
+% the Matlab plot command for a field of arrows is:
+quiver(uMesh, wMesh, f(uMesh, wMesh), g(uMesh,wMesh), 0.5)
+
+plot(X(:,1),X(:,2),'-r')
+plot(X(end,1),X(end,2), 'or')
